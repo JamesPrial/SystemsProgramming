8f8d24002e651f86ebeb1037b8fff20f3459d4d8
JamesPrial <prialjames17@gmail.com>
2024-08-09 12:30:11 -0400

    Delete Asst1 directory

--- Asst1/mymalloc.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef _mymalloc
-#define _mymalloc
-	static char myblock[4096];
-	void * mymalloc(size_t size, char * filleName, int fileNum);
-	void myfree(void * ptr, char * fileName, int fileNum);
-	#define malloc(x) mymalloc(x, __FILE__, __LINE__)
-	#define free(x) myfree(x, __FILE__, __LINE__)
-#endif
